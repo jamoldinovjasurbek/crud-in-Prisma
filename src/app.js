1ea3e8bca6dcaa3b0871ea7d4d20ea2c83817102
jamoldinovjasurbek <117964898+jamoldinovjasurbek@users.noreply.github.com>
2025-04-22 15:18:06 +0500

    updated

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
 import express from "express";
 import "dotenv/config";
 import UserRoute from "./router/user.routes.js";
+import { PrismaClient } from "@prisma/client";
 const Port = process.env.PORT || 8080;
 
 const app = express();
-
+const prisma=new PrismaClient()
 app.use(express.json());
 app.use("/api/users", UserRoute);
 
 const initApp = async () => {
   try {
+    await prisma.$connect()
+    console.log("database connected");
+    
     app.listen(Port, () => {
       console.log("server is running at port:", Port);
     });
